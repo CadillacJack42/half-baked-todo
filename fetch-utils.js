5e7ec6a76048ed8ff1e58d8ed777a165df9ce802
James Keicher <jamesdkeicher@gmail.com>
2022-01-05 17:40:25 -0800

    Completed lab

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,3 +1,4 @@
+
 const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
@@ -5,34 +6,52 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo){
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
+    const response = await client 
+        .from('todos')
+        .insert([{
+            todo,
+            complete: false
+        }]);
     return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
-
+    const response = await client 
+        .from('todos')
+        .delete();
     return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
-
+    const response = await client
+        .from('todos')
+        .select();
     return checkError(response);    
 }
 
-export async function completeTodo(id) {
+export async function completeTodo(todo) {
     // find the and update (set complete to true), the todo that matches the correct id
-
+    let bool;
+    if (todo.complete) {
+        bool = false;
+    } else { 
+        bool = true;
+    }
+    const response = await client
+        .from('todos')
+        .update({ complete: bool })
+        .match({ id: todo.id })
+        .single();
+    console.log(response);
+    
     return checkError(response);    
 }
-
-
 
 export async function getUser() {
     return client.auth.session();
 }
-
 
 export async function checkAuth() {
     const user = await getUser();
@@ -49,13 +68,13 @@ export async function redirectIfLoggedIn() {
 export async function signupUser(email, password){
     const response = await client.auth.signUp({ email, password });
     
-    return checkError(response);
+    return response.user;
 }
 
 export async function signInUser(email, password){
     const response = await client.auth.signIn({ email, password });
 
-    return checkError(response);
+    return response.user;
 }
 
 export async function logout() {
